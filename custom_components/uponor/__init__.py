790fef03ac2ee4b319f84f5e85fd255b59082762
Aivaras <aivaras@sev.lt>
2020-04-04 10:33:31 +0300

    Initial uponor component

---
+++ custom_components/uponor/__init__.py
@@ -0,0 +1,116 @@
+from datetime import timedelta
+import math
+import ipaddress
+import requests
+import voluptuous as vol
+import logging
+
+from homeassistant.const import CONF_HOST, CONF_NAME
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.discovery import async_load_platform
+from homeassistant.helpers.dispatcher import async_dispatcher_send
+from homeassistant.helpers.event import async_track_time_interval
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+
+_LOGGER = logging.getLogger(__name__)
+DOMAIN = "uponor"
+DEFAULT_NAME = "Uponor"
+
+SIGNAL_UPONOR_STATE_UPDATE = "uponor_state_update"
+SCAN_INTERVAL = timedelta(seconds=30)
+
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {
+                vol.Required(CONF_HOST): vol.All(ipaddress.ip_address, cv.string),
+                vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+            }
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
+)
+
+async def async_setup(hass, config):
+    conf = config[DOMAIN]
+    host = conf.get(CONF_HOST)
+    name = conf.get(CONF_NAME)
+
+    state_proxy = UponorStateProxy(hass, host)
+    await state_proxy.async_update(0)
+    thermostats = state_proxy.get_active_thermostats()
+
+    hass.data[DOMAIN] = {"state_proxy": state_proxy, "name": name, "thermostats": thermostats}
+
+    hass.async_create_task(async_load_platform(hass, "climate", DOMAIN, {}, config))
+
+    async_track_time_interval(hass, state_proxy.async_update, SCAN_INTERVAL)
+
+    return True
+
+class UponorStateProxy:
+    def __init__(self, hass, host):
+        self._hass = hass
+        self._host = host
+        self._data = {}
+
+    def get_active_thermostats(self):
+        active = []
+        for i in range(1, 13):
+            var = 'C1_thermostat_' + str(i) + '_presence'
+            if var in self._data and self._data[var] == "1":
+                active.append(i)
+        return active
+
+    def is_heating_active(self, thermostat):
+        var = 'C1_T' + str(thermostat) + '_stat_cb_actuator'
+        if var in self._data:
+            return self._data[var] == "1"
+
+    def is_eco_mode_active(self, thermostat):
+        var = 'C1_T' + str(thermostat) + '_stat_cb_eco_forced'
+        var2 = 'C1_T' + str(thermostat) + '_stat_cb_comfort_eco_mode'
+        if var in self._data or var2 in self._data :
+            return self._data[var] == "1" or self._data[var2] == "1"
+
+    def get_temperature(self, thermostat):
+        var = 'C1_T' + str(thermostat) + '_room_temperature'
+        if var in self._data:
+            return round((int(self._data[var]) - 320) / 18,1)
+
+    def get_humidity(self, thermostat):
+        var = 'C1_T' + str(thermostat) + '_rh'
+        if var in self._data:
+            return int(self._data[var])
+
+    def get_setpoint(self, thermostat):
+        var = 'C1_T' + str(thermostat) + '_setpoint'
+        if var in self._data:
+            return math.floor((int(self._data[var]) - 320) / 1.8) /10
+
+    def set_setpoint(self, thermostat, temp):
+        var = 'C1_T' + str(thermostat) + '_setpoint'
+        setpoint = int(temp * 18 + 320)
+        self.send(var, setpoint)
+        self._data[var] = setpoint
+        async_dispatcher_send(self._hass, SIGNAL_UPONOR_STATE_UPDATE)
+
+    def send(self, var, value):
+        url = "http://" + self._host + "/JNAP/"
+        data = '{"vars": [{"waspVarName": "' + var + '","waspVarValue": "' + str(value) + '"}]}'
+        r = requests.post(url=url, headers={"x-jnap-action": "http://phyn.com/jnap/uponorsky/SetAttributes"}, data=data)
+        j = r.json()
+        if 'results' in j and not j['results'] == 'OK':
+            _LOGGER.error(j)
+
+    async def async_update(self, event_time):
+        url = "http://" + self._host + "/JNAP/"
+        r = requests.post(url=url, headers={"x-jnap-action": "http://phyn.com/jnap/uponorsky/GetAttributes"}, data='{}')
+        j = r.json()
+        vars = {}
+        for v in j['output']['vars']:
+            vars[v['waspVarName']] = v['waspVarValue']
+        self._data = vars
+
+        async_dispatcher_send(self._hass, SIGNAL_UPONOR_STATE_UPDATE)
+
---
+++ custom_components/uponor/climate.py
@@ -0,0 +1,112 @@
+import logging
+
+from homeassistant.components.climate import ClimateDevice
+from homeassistant.core import callback
+from homeassistant.helpers.dispatcher import async_dispatcher_connect
+from homeassistant.components.climate.const import (
+    HVAC_MODE_AUTO,
+    HVAC_MODE_HEAT,
+    HVAC_MODE_OFF,
+    CURRENT_HVAC_OFF,
+    CURRENT_HVAC_HEAT,
+    CURRENT_HVAC_IDLE,
+    SUPPORT_TARGET_TEMPERATURE,
+)
+from homeassistant.const import ATTR_TEMPERATURE, TEMP_CELSIUS
+
+from . import (
+    DOMAIN,
+    SIGNAL_UPONOR_STATE_UPDATE
+)
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    state_proxy = hass.data[DOMAIN]["state_proxy"]
+
+    entities = []
+    for thermostat in hass.data[DOMAIN]["thermostats"]:
+        entities.append(UponorClimate(state_proxy, str(thermostat)))
+    if entities:
+        async_add_entities(entities, update_before_add=False)
+
+class UponorClimate(ClimateDevice):
+
+    def __init__(self, state_proxy, thermostat):
+        self._state_proxy = state_proxy
+        self._thermostat = thermostat
+        self._is_on = True
+        self._last_temp = 20
+
+    @property
+    def name(self):
+        return "Climate [T" + self._thermostat + "]"
+
+    @property
+    def should_poll(self):
+        return False
+
+    async def async_added_to_hass(self):
+        async_dispatcher_connect(
+            self.hass, SIGNAL_UPONOR_STATE_UPDATE, self._update_callback
+        )
+
+    @callback
+    def _update_callback(self):
+        self.is_on = self._state_proxy.get_temperature(self._thermostat) > 7
+        self.async_schedule_update_ha_state(True)
+
+    @property
+    def supported_features(self):
+        if self._is_on:
+            return SUPPORT_TARGET_TEMPERATURE
+        return 0
+
+    @property
+    def hvac_action(self):
+        if not self._is_on:
+            return CURRENT_HVAC_OFF
+        if self._state_proxy.is_heating_active(self._thermostat):
+            return CURRENT_HVAC_HEAT
+        return CURRENT_HVAC_IDLE
+
+    @property
+    def hvac_mode(self):
+        if self._is_on:
+            return HVAC_MODE_HEAT
+        return HVAC_MODE_OFF
+
+    def set_hvac_mode(self, hvac_mode):
+        if hvac_mode == HVAC_MODE_OFF and self._is_on:
+            self._last_temp = self.target_temperature
+            self._state_proxy.set_setpoint(self._thermostat, 7)
+            self._is_on = False
+        if hvac_mode == HVAC_MODE_HEAT and not self._is_on:
+            self._state_proxy.set_setpoint(self._thermostat, self._last_temp)
+            self._is_on = True
+
+    @property
+    def hvac_modes(self):
+        return [HVAC_MODE_HEAT, HVAC_MODE_OFF]
+
+    @property
+    def temperature_unit(self):
+        return TEMP_CELSIUS
+
+    @property
+    def current_temperature(self):
+        return self._state_proxy.get_temperature(self._thermostat)
+
+    @property
+    def current_humidity(self):
+        return self._state_proxy.get_humidity(self._thermostat)
+
+    @property
+    def target_temperature(self):
+        return self._state_proxy.get_setpoint(self._thermostat)
+
+    def set_temperature(self, **kwargs):
+        temp = kwargs.get(ATTR_TEMPERATURE)
+        if temp is not None and self._is_on:
+            self._state_proxy.set_setpoint(self._thermostat, temp)
+
