db84fdd310b98306b4873264ae0785614220193c
Dave Code <34915602+dave-code-ruiz@users.noreply.github.com>
2024-07-09 08:52:24 +0200

    trying to solves deprecated async_add_job

--- custom_components/uponor/__init__.py
+++ custom_components/uponor/__init__.py
@@ -225,7 +225,7 @@ class UponorStateProxy:
         
         await self._hass.async_add_executor_job(lambda: self._client.send_data({'sys_heat_cool_mode': '1'}))
         self._data['sys_heat_cool_mode'] = '1'
-        self._hass.async_add_job(async_dispatcher_send, self._hass, SIGNAL_UPONOR_STATE_UPDATE)
+        self._hass.async_create_task(async_dispatcher_send(self._hass, SIGNAL_UPONOR_STATE_UPDATE))
 
     async def async_switch_to_heating(self):
         for thermostat in self._hass.data[DOMAIN]['thermostats']:
@@ -234,7 +234,7 @@ class UponorStateProxy:
 
         await self._hass.async_add_executor_job(lambda: self._client.send_data({'sys_heat_cool_mode': '0'}))
         self._data['sys_heat_cool_mode'] = '0'
-        self._hass.async_add_job(async_dispatcher_send, self._hass, SIGNAL_UPONOR_STATE_UPDATE)
+        self._hass.async_create_task(async_dispatcher_send(self._hass, SIGNAL_UPONOR_STATE_UPDATE))
 
     async def async_turn_on(self, thermostat):
         data = await self._store.async_load()
@@ -273,7 +273,7 @@ class UponorStateProxy:
         data = "1" if is_away else "0"
         await self._hass.async_add_executor_job(lambda: self._client.send_data({var: data}))
         self._data[var] = data
-        self._hass.async_add_job(async_dispatcher_send, self._hass, SIGNAL_UPONOR_STATE_UPDATE)
+        self._hass.async_create_task(async_dispatcher_send(self._hass, SIGNAL_UPONOR_STATE_UPDATE))
 
     def is_eco(self, thermostat):
         if self.get_eco_setback(thermostat) == 0:
@@ -296,7 +296,7 @@ class UponorStateProxy:
         try:
             self.next_sp_from_dt = dt_util.now()
             self._data = await self._hass.async_add_executor_job(lambda: self._client.get_data())
-            self._hass.async_add_job(async_dispatcher_send, self._hass, SIGNAL_UPONOR_STATE_UPDATE)
+            self._hass.async_create_task(async_dispatcher_send(self._hass, SIGNAL_UPONOR_STATE_UPDATE))
         except Exception as ex:
             _LOGGER.error("Uponor thermostat was unable to update: %s", ex)
         
@@ -304,11 +304,11 @@ class UponorStateProxy:
         _LOGGER.debug("Called set variable: name: %s, value: %s, data: %s", var_name, var_value, self._data)
         self._client.send_data({var_name: var_value})
         self._data[var_name] = var_value
-        self._hass.async_add_job(async_dispatcher_send, self._hass, SIGNAL_UPONOR_STATE_UPDATE)
+        self._hass.async_create_task(async_dispatcher_send(self._hass, SIGNAL_UPONOR_STATE_UPDATE))
 
     async def set_setpoint(self, thermostat, temp):
         var = thermostat + '_setpoint'
         setpoint = int(temp * 18 + self.get_active_setback(thermostat, temp) + 320)
         await self._hass.async_add_executor_job(lambda: self._client.send_data({var: setpoint}))
         self._data[var] = setpoint
-        self._hass.async_add_job(async_dispatcher_send, self._hass, SIGNAL_UPONOR_STATE_UPDATE)
+        self._hass.async_create_task(async_dispatcher_send(self._hass, SIGNAL_UPONOR_STATE_UPDATE))
